83c8e80d568a6c4452ecc990ee46d4caaa172262
Hong Tat Tan <HongTat.Tan@CGG.com>
2020-08-22 14:48:04 +0800

    fix

--- root/etc/cont-init.d/10_init.sh
+++ root/etc/cont-init.d/10_init.sh
@@ -1,8 +1,23 @@
 #!/usr/bin/with-contenv bash
 
+echo "Enabling Post-Service"
+mkdir -p /etc/services.d/post-service/
+cat <<EOT > /etc/services.d/post-service/run
+#!/usr/bin/env bash
+SUPERVISED_DIR=/var/run/s6/services/
+
+if ls -ald /etc/services.d/* &>/dev/null; then
+  for service in /etc/services.d/*; do
+    svc=$(basename "${service}")
+    if [ "${svc}" != "post-service" ]; then
+      echo "[post-service] ensure ${svc} is ready"
+      s6-svwait -u "${SUPERVISED_DIR}/${svc}"
+    fi
+  done
+  echo "[post-service] all services up"
+fi
 RUN_SCRIPTS=/etc/run1.d
 STATUS=0
-
 # Run shell scripts (ending in .sh) in run.d directory
 
 if ls ${RUN_SCRIPTS}/*.sh &>/dev/null; then
@@ -28,3 +43,9 @@ if ls ${RUN_SCRIPTS}/*.sh &>/dev/null; then
 else
   echo "[init] no run1.d scripts"
 fi
+s6-svc -x ${SUPERVISED_DIR}/post-service
+rm -rf ${SUPERVISED_DIR}/post-service
+s6-svscanctl -a ${SUPERVISED_DIR}
+echo "[post-service] complete"
+EOT
+chmod +x /etc/services.d/post-service/run
